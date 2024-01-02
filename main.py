8e9c61fb9dddb043ec66fc8cf82cc405c4c48e2e
User <igorspbschool@yandex.ru>
2024-01-02 07:39:05 +0300

    add/updata new sort

--- main.py
+++ main.py
@@ -21,6 +21,24 @@ class MyWidget(QMainWindow):
         column_widths = [40, 70, 70, 80, 380, 60, 100]
         for col, width in enumerate(column_widths):
             self.tableWidget.setColumnWidth(col, width)
+        self.zagruzka_data()
+
+    def zagruzka_data(self):
+        connection = sqlite3.connect('coffee.sqlite')
+        cursor = connection.cursor()
+        cursor.execute(
+            "SELECT ID, name_sort, degree_roasting, ground_beans,\
+             description_taste, "
+            "price, packaging_volume FROM coffee")
+        data = cursor.fetchall()
+        self.tableWidget.setRowCount(len(data))
+
+        for i, row in enumerate(data):
+            for j, value in enumerate(row):
+                item = QTableWidgetItem(str(value))
+                self.tableWidget.setItem(i, j, item)
+
+        connection.close()
 
 
 if __name__ == '__main__':
